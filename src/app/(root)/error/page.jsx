66cdc6c2c39e1a9d7a388f98bb4ebdb46003a502
charles1stgithub <charleslester.official@gmail.com>
2025-02-11 17:50:58 +0800

    Fixed Error Page

--- src/app/(root)/error/page.jsx
+++ src/app/(root)/error/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Page = () => {
-  return <div>Error</div>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-4xl font-bold">Error</h1>
+    </div>
+  );
 };
 
 export default Page;
